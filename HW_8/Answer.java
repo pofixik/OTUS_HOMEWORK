ddf1c13a94fdc2d15b78055f46e1baf7f8c0a156
pofixik <fedalice@yandex.ru>
2022-08-17 00:46:28 +0300

    process

--- HW_8/Answer.java
+++ HW_8/Answer.java
@@ -1,12 +1,24 @@
 package HW_8;
 
 class Answer {
-   private String text;
-    public Answer(String text){
-        this.text = text;
+    private String text;
+
+
+    public void setAnswer(String answer) {
+        this.text  = answer;
     }
-    public  void showText() {
-        System.out.println(text);
+    public void showText() {
+        System.out.println(". " +text);
     }
 
+
+    private TestElement testElement;
+
+    public void setTestElement(TestElement testElement) {
+        this.testElement  = testElement;
     }
+
+    public TestElement getTestElement() {
+        return testElement;
+    }
+}
--- HW_8/TestElement.java
+++ HW_8/TestElement.java
@@ -6,6 +6,8 @@ public class TestElement {
     private int rightAnswerIndex;
     private String question;
 
+    int count=0;
+
     public TestElement(String question, int max, int rightAnswerIndex){
         this.max=max;
         this.question=question;
@@ -13,14 +15,28 @@ public class TestElement {
         this.answers=new Answer[max];
 
     }
+    private Test test;
 
-    public void addAnswer(String answerText) {
-         Answer a = new Answer(answerText);
+    public void setTest(Test test) {
+        this.test  = test;
     }
+
+    public Test getTest() {
+        return test;
+    }
+    public void addAnswer(Answer newAnswer, String text) {
+        newAnswer.setAnswer(text);
+        newAnswer.setTestElement(this);
+        answers[count++]=newAnswer;
+    }
+
+
     public void showAnswers(){
         for (int i=0; i<max;i++)
         {
-            System.out.println(i+". "+ answers[i]);
+            System.out.print(i+1);
+            answers[i].showText();
+
         }
 
     }
@@ -28,11 +44,16 @@ public class TestElement {
 
 
     public boolean ask(){
+        System.out.println();
         System.out.println(question);
-        System.out.println("Варианты ответов: ");
-        showAnswers();
 
+        System.out.println("Варианты ответов: ");
+        System.out.println();
+        showAnswers();
 
         return true;
     }
+
+
+
 }
